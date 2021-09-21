c3596587a658be27a2c233b85e5039fabd00ede7
carlosyanez <carlos.yanez.s@gmail.com>
2021-09-21 13:19:52 +0200

    adding vignette plus pkgdown page

---
+++ README.md
@@ -0,0 +1,32 @@
+<img src="https://github.com/carlosyanez/just.install/raw/main/img/hexSticker.png" width = "175" height = "200" align="right" border=0 style="border:0; text-decoration:none; outline:none"/>
+
+
+# just.install
+Simple utility to *just* install R packages.
+
+## Install
+You can install this template with:
+
+```
+ devtools::install_github("carlosyanez/just.install")
+ ```
+ 
+Alternatively, you can install it from [r-universe](https://r-universe.dev/) 
+
+```
+  options(repos = c(
+          carlosyanez = 'https://carlosyanez.r-universe.dev',
+          CRAN = 'https://cloud.r-project.org'))
+
+  install.packages('just.install')
+```
+## How to use 
+
+[How to use this template](https://carlosyanez.github.io/uofgdataanalyticsreport/articles/how_to_use.html)
+
+
+## Issues? bugs? Ideas?
+
+If you find something that is not quite right, please post an issue.
+If you have any ideas, or want to **collaborate** please let me know!
+
---
+++ inst/doc/how_to_use.R
@@ -0,0 +1,21 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+    echo=TRUE,
+    eval=FALSE,
+    comment = "#>"
+)
+
+## -----------------------------------------------------------------------------
+#  packages_to_install <- tibble::tribble(~package,       ~source,~url,
+#                                    "librarian",    "Github","DesiQuintans/librarian",
+#                                    "box",          "CRAN","",
+#                                    "randomForest", "CRAN","",    # for RandomForest
+#                                    "here",         "CRAN","",
+#                                    "fs",           "CRAN","",
+#                                    "aussiempas",    "r-universe","https://carlosyanez.r-universe.dev"
+#                                    )
+#  
+
+## -----------------------------------------------------------------------------
+#  just.install::justinstall(packages_to_install)
+
