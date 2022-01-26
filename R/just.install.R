bf5c47c138e238d04273d018060aabd632a8163f
carlosyanez <carlos.yanez.s@gmail.com>
2022-01-26 10:20:50 +0100

    v 1

--- R/just.install.R
+++ R/just.install.R
@@ -4,15 +4,12 @@
 #' It does not attach any packages, existing or to be installed.
 #'
 #' @return no output
-#' @import tibble
-#' @import dplyr
-#' @import magrittr
-#' @import tools
-#' @importFrom BiocManager install
-#' @importFrom  remotes install_github
-#' @importFrom utils  install.packages installed.packages
-#' @param to_install tibble or data.frame with packages to install (attr: package: package name, source: {"CRAN","Github","Bioconductor","Other"}, url: username/package for Github, repo url for Other)
-#' @param cran_repo_option CRAN repository. Defaults to system options
+#' @importFrom dplyr mutate case_when
+#' @importFrom magrittr %>%
+#' @import remotes
+#' @importFrom utils  installed.packages
+#' @importFrom stringr str_c
+#' @param to_install tibble or data.frame with packages to install (see vignette for details)
 #' @examples
 #' \dontrun{
 #' to_install <- tibble::tibble(package=c("tidyverse","ochRe","customthemes"),
@@ -22,64 +19,95 @@
 #' just.install::justinstall(to_install)
 #' }
 #' @export justinstall
-justinstall <- function(to_install,
-                        cran_repo_option=getOption('repos')){
+justinstall <- function(to_install){
 
-  installed_packages  <- rownames(installed.packages())
+  #to comply with rmd checks
+  source_type <- NULL
 
-  there <- to_install[to_install$package %in% installed_packages,]
+  # get list of installed packages
+  installed_packages  <- utils::installed.packages()[,1]
+
+  #determined which packages have been installed, which ones to install
+  there     <- to_install[to_install$package %in% installed_packages,]
   missing   <- to_install[!(to_install$package %in% installed_packages),]
 
-  # these packages are already installed
+  #main installation logic
   if(nrow(there)==0){
     message("None installed")
   }
   else{
-  for(i in 1:nrow(there)){
-    message(there[i,]$package," already installed")
-  }
-  }
-
-  # installing new packages
-  if(nrow(missing)>0){
-
-
-    for(i in 1:nrow(missing)){
-      if(missing[i,]$source=='CRAN'){
-        message('Installing ',missing[i,]$package,' from default repos')
-        install.packages(missing[i,]$package,
-                       #  dependencies=dependencies_option,
-                         repos = cran_repo_option)  # classic installation from CRAN
-      }else{
-        if(missing[i,]$source %in% c("Bioc","Bioconductor","BioConductor")){
-          message('Installing ',missing[i,]$package,' from BioConductor')
-          BiocManager::install(missing[i,]$package) #,
-                            #   dependencies = dependencies_option)                                # Bioconductor
-        }else{
-          if(missing[i,]$source %in% c("Github","GitHub","github","gh")){
-            message('Installing ',missing[i,]$package,' from Github')
-            remotes::install_github(missing[i,]$url) #
-                         # ,dependencies = dependencies_option)                               #Github repository
-
-          }else{
-            if(missing[i,]$source %in% c("mini-cran","r-universe")){
-              message("installing ",missing[i,]$package," from ",missing[i,]$url)
-              install.packages(missing[i,]$package,repos=c(missing[i,]$url))#,dependencies = dependencies_option)          # mini-cran, r-universe style repo
-
-              }else{message("I don't know how to install ",missing[i,]$package)}
-
-          }
-        }
-
-      }
+    # list all packages already installed
+    for(i in 1:nrow(there)){
+      message(there[i,]$package," already installed")
     }
-    message("Task done. Goodbye!")
+    if(nrow(missing)==0){
+      message(("Nothing to install"))
+    }
+    else{
+
+      #standardise source names
+      # fill bioconductor if source_type is empty
+      missing <- missing %>%
+                 dplyr::mutate(source_type=tolower(source_type),
+                        source_type=dplyr::case_when(
+                                      source_type=="gh"           ~ 'github',
+                                      source_type=="universe"     ~ 'r-universe',
+                                      source_type=="bioconductor" ~ 'bioc',
+                                      TRUE ~ source_type
+                                      )
+                        ) %>%
+                  dplyr::mutate(
+                        source_param = dplyr::case_when(
+                                      source_type=="bioc" & source_param=="" ~ source_type,
+                                      TRUE ~ source_param
+                                      )
+                        )
+
+
+      # installing new packages
+      for(i in 1:nrow(missing)){
+        install_package(missing[i,]$package,missing[i,]$source,missing[i,]$url)
+      }
+      message("Task complete Goodbye!")
+    }
   }
-
-
 
 }
 
+
+#' internal function to that installs a single package based on data provided
+#' @return no output
+#' @param  package_name package name
+#' @param  source_type  where it should be installed from
+#' @param  source_param repository source, where it applies
+#' @import remotes
+#' @importFrom stringr str_c
+install_package <- function(package_name,source_type,source_param){
+
+  ### use {remotes} function based on source type
+
+  message(stringr::str_c("Installing ", package_name, " from", source_type))
+
+  switch(source_type,
+         "bioc"       = {remotes::install_bioc(package_name)},
+         "bitbucket"  = {remotes::install_bitbucket(source_param)},
+         "cran"       = {remotes::install_cran(package_name)},
+         "dev"        = {remotes::install_dev(package_name)},
+         "git"        = {remotes::install_git(source_param)},
+         "github"     = {remotes::install_github(source_param)},
+         "gitlab"     = {remotes::install_gitlab(source_param)},
+         "local"      = {remotes::install_local(source_param)},
+         "svn"        = {remotes::install_svn(source_param)},
+         "url"        = {remotes::install_url(source_param)},
+         "r-universe" = {remotes::install_cran(package_name,repos=c(source_param))}
+
+  )
+
+
+  message(stringr::str_c(package_name, " installed"))
+
+
+}
 
 
 
